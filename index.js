1bf38f7a7f4c03f6f6e553967d3383d617ae2ec5
hanaut <d-hanada@ut-g.co.jp>
2019-03-12 11:07:55 +0900

    サンプル

--- index.js
+++ index.js
@@ -1,83 +1,49 @@
 //index.js
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-//server.listen(process.env.PORT || 3000);
+// create LINE SDK client
+const client = new line.Client(config);
 
-//起動確認
-server.listen(process.env.PORT || 3000,  () => {
-  console.log(`Server running`);
-});
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
 
-// APIコールのためのクライアントインスタンスを作成
-//const bot = new line.Client(line_config);
-const client = new line.Client({
-  channelAccessToken: '<channel access token>'
-});
-
-const message = {
-  type: 'text',
-  text: 'Hello World!'
-};
-
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
-
-//    // すべてのイベント処理のプロミスを格納する配列。
-//    let events_processed = [];
-
-
-    client.replyMessage('<replyToken>', message)
-      .then(() => {
-        ...
-      })
-      .catch((err) => {
-        // error handling
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/bot/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
     });
-
-
-//    // イベントオブジェクトを順次処理。
-//    req.body.events.forEach((event) => {
-//       // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-//        if (event.type == "message" && event.message.type == "text"){
-//            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-//            if (event.message.text == "こんにちは"){
-//                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-//                events_processed.push(bot.replyMessage(event.replyToken, {
-//                    "type": "text",
-//                    "text": "これはこれは"
-//                }));
-//            } else {
-//                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-//                events_processed.push(bot.replyMessage(event.replyToken, {
-//                    "type": "text",
-//                    "text": "はい？耳が遠いもんで。なんですか？"
-//                }));
-//            }
-//        }
-//    });
-//
-//    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-//    Promise.all(events_processed).then(
-//        (response) => {
-//            console.log(`${response.length} event(s) processed.`);
-//        }
-//    );
 });
 
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
 
