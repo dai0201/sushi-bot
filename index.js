1c38ee0b55d1e4b20baab1480dd9aabdf6158fda
hanaut <d-hanada@ut-g.co.jp>
2019-03-12 10:52:34 +0900

    re

--- index.js
+++ index.js
@@ -33,50 +33,50 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
 
-//    // すべてのイベント処理のプロミスを格納する配列。
-//    let events_processed = [];
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
 
-    const message = {
-      type: 'text',
-      text: 'Hello World!'
-    };
+//    const message = {
+//      type: 'text',
+//      text: 'Hello World!'
+//    };
 
-    client.replyMessage('<replyToken>', message)
-      .then(() => {
-        ...
-      })
-      .catch((err) => {
-        // error handling
+//    client.replyMessage('<replyToken>', message)
+//      .then(() => {
+//        ...
+//      })
+//      .catch((err) => {
+//        // error handling
+//    });
+
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+       // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    "type": "text",
+                    "text": "これはこれは"
+                }));
+            } else {
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    "type": "text",
+                    "text": "はい？耳が遠いもんで。なんですか？"
+                }));
+            }
+        }
     });
 
-
-//    // イベントオブジェクトを順次処理。
-//    req.body.events.forEach((event) => {
-//       // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-//        if (event.type == "message" && event.message.type == "text"){
-//            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-//            if (event.message.text == "こんにちは"){
-//                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-//                events_processed.push(bot.replyMessage(event.replyToken, {
-//                    "type": "text",
-//                    "text": "これはこれは"
-//                }));
-//            } else {
-//                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-//                events_processed.push(bot.replyMessage(event.replyToken, {
-//                    "type": "text",
-//                    "text": "はい？耳が遠いもんで。なんですか？"
-//                }));
-//            }
-//        }
-//    });
-//
-//    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-//    Promise.all(events_processed).then(
-//        (response) => {
-//            console.log(`${response.length} event(s) processed.`);
-//        }
-//    );
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
 
 
